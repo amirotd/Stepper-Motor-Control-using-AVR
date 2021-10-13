f2d00cb02ce80ba0acceab2df7c7b5535515e83a
amir otd <amir.otd.1@gmail.com>
2021-10-13 16:40:20 +0200

    added stepper.c source code

---
+++ src/stepper.c
@@ -0,0 +1,382 @@
+#include <mega8.h>
+#include <delay.h>
+
+long duty = 50;
+int waitMicroSeconds = 5000;
+int pulseCount = 50;
+
+
+void setSpeed(int wms , int pc);
+void doStep(int st);
+void motorOff(void);
+void fullStep(int cnt,char forwards);
+void halfStep(int cnt,char forwards);
+void u16Step(int cnt, char forwards);
+void u24Step(int cnt, char forwards);
+void u32Step(int cnt, char forwards);
+void u48Step(int cnt, char forwards);
+void u64Step(int cnt, char forwards);
+void u96Step(int cnt, char forwards);
+
+void main(void)
+{
+
+PORTB=0x01;
+DDRB=0x01;
+
+PORTC=0x00;
+DDRC=0x0F;
+
+setSpeed(5000,50);
+
+while (1)
+      {
+       fullStep(1,'R');
+       halfStep(1,'R');
+       u16Step(1,'L');
+       u24Step(1,'R');
+       u32Step(1,'R');
+       u48Step(1,'R');
+       u64Step(1,'R');
+       u96Step(1,'R');
+       motorOff(); 
+       delay_ms(6);
+      }
+}
+
+void setSpeed(int wms , int pc){
+    waitMicroSeconds = wms;
+    pulseCount = pc;
+}
+
+void doStep(int st){
+  int k=0;
+  long dt1 = (waitMicroSeconds * duty / 100)/1000;
+  long dt2 = (waitMicroSeconds * (100-duty) / 100)/1000;
+ 
+  for (k = 0; k < pulseCount; k++){
+    switch (st){
+    case 1: PORTC=0x05;break;
+    case 2: PORTC=0x09;break;
+    case 3: PORTC=0x0A;break;
+    case 4: PORTC=0x06;break;
+    case 11: PORTC=0x01;break;
+    case 12: PORTC=0x08;break;
+    case 13: PORTC=0x02;break;
+    case 14: PORTC=0x04;break;
+
+    case 21: PORTC=0x05;break;
+    case 22: PORTC=0x09;break;
+    case 23: PORTC=0x0A;break;
+    case 24: PORTC=0x06;break;
+    case 31: PORTC=0x01;break;
+    case 32: PORTC=0x08;break;
+    case 33: PORTC=0x02;break;
+    case 34: PORTC=0x04;break;;
+    }
+
+    delay_ms(dt1);
+    switch (st){
+    case 1: PORTC=0x05;break;
+    case 2: PORTC=0x09;break;
+    case 3: PORTC=0x0A;break;
+    case 4: PORTC=0x06;break;
+    case 11: PORTC=0x01;break;
+    case 12: PORTC=0x08;break;
+    case 13: PORTC=0x02;break;
+    case 14: PORTC=0x04;break;
+
+    case 21: PORTC=0x01;break;
+    case 22: PORTC=0x08;break;
+    case 23: PORTC=0x02;break;
+    case 24: PORTC=0x04;break;
+    case 31: PORTC=0x09;break;
+    case 32: PORTC=0x0A;break;
+    case 33: PORTC=0x06;break;
+    case 34: PORTC=0x05;break;
+
+    }
+
+    delay_ms(dt2);    
+  }
+}
+
+void motorOff(void){
+  PORTC = 0x00;   
+}
+
+void fullStep(int cnt, char forwards){
+  int i=0;
+  for (i = 0; i < cnt; i++){
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 1; j <= 4; j++){
+        doStep(j);
+        }
+      }
+    else if (forwards == 'L')
+      {
+      int j=0;
+      for (j = 4; j >= 1; j--){
+        doStep(j);
+        }
+      }
+    }
+}
+
+void halfStep(int cnt, char forwards){
+  const int list[] = {1,11,2,12,3,13,4,14};
+  int i=0;
+  for (i = 0; i < cnt; i++){
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 7; j++){
+        doStep(list[j]);
+        }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 7; j >= 0; j--){
+        doStep(list[j]);
+        }
+      }
+  }
+}
+
+void u16Step(int cnt, char forwards){
+
+  const int list[] = {1,21,11,31,2,22,12,32,3,23,13,33,4,24,14,34};
+  int i=0;
+  for (i = 0; i < cnt; i++){
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 15; j++){
+        doStep(list[j]);
+        }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 15; j >= 0; j--){
+        doStep(list[j]);
+        }
+      }
+  }  
+}
+  
+void u24Step(int cnt, char forwards){
+  const int list1[] = {1,11,2,12,3,13,4,14};
+  const int list2[] = {21,31,22,32,23,33,24,34};
+   int i=0;
+  for (i = 0; i < cnt; i++){
+
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 7; j++){
+        doStep(list1[j]);
+          if (forwards == 'R'){
+            duty = 66;   doStep(list2[j]);
+            duty = 33;   doStep(list2[j]);
+        }
+          else if(forwards == 'L'){
+            duty = 33;   doStep(list2[j]);
+            duty = 66;   doStep(list2[j]);
+        }
+      }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 7; j >= 0; j--){
+      if (forwards == 'R'){
+            duty = 66;   doStep(list2[j]);
+            duty = 33;   doStep(list2[j]);
+        }
+          else if(forwards == 'L'){
+            duty = 33;   doStep(list2[j]);
+            duty = 66;   doStep(list2[j]);
+        }
+        doStep(list1[j]);
+        }
+      }
+     
+  }
+}
+void u32Step(int cnt, char forwards){
+  const int list1[] = {1,11,2,12,3,13,4,14};
+  const int list2[] = {21,31,22,32,23,33,24,34};
+   int i=0;
+  for (i = 0; i < cnt; i++){
+
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 7; j++){
+      doStep(list1[j]); 
+      if (forwards == 'R'){
+        duty = 75;    doStep(list2[j]);
+        duty = 50;    doStep(list2[j]);
+        duty = 25;    doStep(list2[j]);
+        }
+      else if (forwards == 'L'){
+        duty = 25;    doStep(list2[j]);
+        duty = 50;    doStep(list2[j]);
+        duty = 75;    doStep(list2[j]);
+      }
+      }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 7; j >= 0; j--){
+      if (forwards == 'R'){
+        duty = 75;    doStep(list2[j]);
+        duty = 50;    doStep(list2[j]);
+        duty = 25;    doStep(list2[j]);
+        }
+      else if (forwards == 'L'){
+        duty = 25;    doStep(list2[j]);
+        duty = 50;    doStep(list2[j]);
+        duty = 75;    doStep(list2[j]);
+      }
+      doStep(list1[j]);
+      }
+      }
+     
+  }
+}
+
+void u48Step(int cnt, char forwards){
+  const int list1[] = {1,11,2,12,3,13,4,14};
+  const int list2[] = {21,31,22,32,23,33,24,34};
+  long i=0;
+  for (i = 0; i < cnt; i++){
+
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 7; j++){
+        doStep(list1[j]); 
+        if (forwards == 'R'){
+          for (i = 5; i >= 1; i--){duty = 17 * i; doStep(list2[j]);}
+        }
+        else if(forwards == 'L'){
+          for (i = 1; i <= 5; i++){duty = 17 * i; doStep(list2[j]);}
+        }
+        }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 7; j >= 0; j--){
+        if (forwards == 'R'){
+          for (i = 5; i >= 1; i--){duty = 17 * i; doStep(list2[j]);}
+        }
+        else if(forwards == 'L'){
+          for (i = 1; i <= 5; i++){duty = 17 * i; doStep(list2[j]);}
+        } 
+        doStep(list1[j]);
+        }
+      }
+     
+  }
+}
+
+void u64Step(int cnt, char forwards){
+  const int list1[] = {1,11,2,12,3,13,4,14};
+  const int list2[] = {21,31,22,32,23,33,24,34};
+  long i=0;
+  for (i = 0; i < cnt; i++){
+
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 7; j++){
+        doStep(list1[j]); 
+        if (forwards == 'R'){
+          for (i = 7; i >= 1; i--){duty = 12 * i; doStep(list2[j]);}
+        }
+        else if(forwards == 'L'){
+          for (i = 1; i <= 7; i++){duty = 12 * i; doStep(list2[j]);}
+        }
+        }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 7; j >= 0; j--){
+        if (forwards == 'R'){
+          for (i = 7; i >= 1; i--){duty = 12 * i; doStep(list2[j]);}
+        }
+        else if(forwards == 'L'){
+          for (i = 1; i <= 7; i++){duty = 12 * i; doStep(list2[j]);}
+        } 
+        doStep(list1[j]);
+        }
+      }
+     
+  }
+}     
+
+void u96Step(int cnt, char forwards){
+  const int list1[] = {1,11,2,12,3,13,4,14};
+  const int list2[] = {21,31,22,32,23,33,24,34};
+  long i=0;
+  //int i=0;
+  for (i = 0; i < cnt; i++){
+
+    duty = 50;
+    if (forwards == 'R')
+      {
+      int j=0;
+      for (j = 0; j <= 7; j++){
+        doStep(list1[j]); 
+        if (forwards == 'R'){
+        //int i=0;
+        for (i = 9; i >= 1; i--){
+         duty = 10 * i; doStep(list2[j]);
+        }
+       }
+        else if(forwards == 'L'){
+        //int i=0;
+        for (i = 1; i <= 9; i++){
+        duty = 10 * i; doStep(list2[j]);
+        }
+       }
+      }
+      }
+    else if(forwards == 'L')
+      {
+      int j=0;
+      for (j = 7; j >= 0; j--){
+        long i=0;
+        if (forwards == 'R'){
+        //int i=0;
+        for (i = 9; i >= 1; i--){
+         duty = 10 * i; doStep(list2[j]);
+        }
+       }
+        else if(forwards == 'L'){
+        //int i=0;
+        for (i = 1; i <= 9; i++){
+        duty = 10 * i; doStep(list2[j]);
+        }
+       } 
+        doStep(list1[j]);
+        }
+      }
+     
+  }
+}    
